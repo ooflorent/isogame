4732d2a661bed0dc5ee69339452ced0be7f54117
Florent Cailhol <florent.cailhol@gmail.com>
2014-05-09 09:25:45 +0200

    Clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 var gulp = require('gulp');
 var component = require('gulp-component-builder');
+var htmlmin = require('gulp-htmlmin');
+
+var htmlminOptions = {
+  collapseWhitespace: true,
+  collapseBooleanAttributes: true,
+  removeAttributeQuotes: true,
+  removeComments: true,
+  removeEmptyAttributes: true,
+  removeOptionalTags: true
+};
+
+gulp.task('index', function() {
+  return gulp.src('index.html')
+    .pipe(htmlmin(htmlminOptions))
+    .pipe(gulp.dest('build'));
+});
 
 gulp.task('scripts', function() {
   return gulp.src('component.json')
@@ -14,11 +30,12 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', ['build'], function() {
+  gulp.watch(['index.html'], ['index']);
   gulp.watch(['component.json', 'lib/**/*.js', 'src/**/*.js'], ['scripts']);
   gulp.watch(['component.json', 'lib/**/*.less', 'src/**/*.less'], ['styles']);
 });
 
-gulp.task('build', ['scripts', 'styles']);
+gulp.task('build', ['index', 'scripts', 'styles']);
 gulp.task('default', ['build']);
 
 var less = require('builder-less');
@@ -27,18 +44,11 @@ var minifier = require('builder-html-minifier');
 function scriptsPlugins(builder, options) {
   builder
     .use('scripts', component.plugins.js())
-    .use('json', component.plugins.json());
-
-  builder.use('templates', minifier({
-    collapseWhitespace: true,
-    collapseBooleanAttributes: true,
-    removeAttributeQuotes: true,
-    removeComments: true,
-    removeEmptyAttributes: true,
-    removeOptionalTags: true
-  }));
+    .use('json', component.plugins.json())
+    .use('templates', minifier(htmlminOptions));
 }
 
 function stylesPlugins(builder, options) {
-  builder.use('styles', less());
+  builder
+    .use('styles', less());
 }
